1f10017a057f7bb7dc1ac393c95600b99e0cf8d3
Erwin Purnama <zzerwinp@bankmayapada.local>
2023-07-07 17:00:37 +0700

    firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'login', component: UserLoginComponent },
+  { path:'forgot-password', component: ForgotPasswordComponent },
+  { path:'', redirectTo: '/login',pathMatch: 'full'},
+  { path:'**', component: NotFoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundPageComponent,
+    UserLoginComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
